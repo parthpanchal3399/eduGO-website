94ca1b9cc9adaad9e5a1815db7119fd505882a67
Parth Panchal <51913848+HtraP1001@users.noreply.github.com>
2019-08-14 16:59:56 +0530

    Structure created

---
+++ src/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>eduGO</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicons -->
+    
+
+    <!-- Google Fonts -->
+    
+
+    <!-- Bootstrap CSS File -->
+    <link href="lib/bootstrap/css/bootstrap-grid.min.css" rel="stylesheet">
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    
+
+    <!-- Main Stylesheet File -->
+    <link href="lib/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!--==========================
+                Header
+    ============================-->
+    <header id="header" style="">
+
+
+
+    </header>
---
+++ src/login.php
@@ -0,0 +1,33 @@
+<?php
+    require "header.php"
+?>
+
+<div class="container-fluid">
+    <div class="website-logo">
+        <a href="index.php">
+            <div>
+                <img height="50px" src="images/logoLCO.svg" alt="logo">
+
+            </div>
+        </a>
+    </div>
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="img-holder">
+                <div class="bg"></div>
+                <div class="info-holder">
+                    <img src="images/key.svg" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-8"> <!-- Login Section -->
+            
+        </div>
+    </div>
+</div>
+
+
+<?php
+    require "footer.php"
+?>
