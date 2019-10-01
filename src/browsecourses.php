fdc0d17cd64a411d1917125982aff14997181a77
Mayur <veerapathiramoorthy.pillai@sakec.ac.in>
2019-10-01 14:10:52 +0530

    browsecourse structure done

---
+++ src/browsecourses.php
@@ -0,0 +1,59 @@
+<?php
+    require "header.php"
+?>
+
+<?php
+        require "navbar.php"
+?>
+
+
+<div class="row" id="course-heading">
+
+</div>
+
+
+<div id="course-main-section">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="course-about-heading"> 
+                <h1>Courses</h1>
+            </div>
+        </div>
+        <div class="row center">
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                <img height="150px" class="card-img-top" src="images/hitesh.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">HTML Crash Course</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Step One for you to become the Creator on Web.</h6>
+                        <a href="#" class="card-link">Learn More</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                <img height="150px" class="card-img-top" src="images/hitesh.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">HTML Crash Course</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Step One for you to become the Creator on Web.</h6>
+                        <a href="#" class="card-link">Learn More</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                <img height="150px" class="card-img-top" src="images/hitesh.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">HTML Crash Course</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Step One for you to become the Creator on Web.</h6>
+                        <a href="#" class="card-link">Learn More</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
