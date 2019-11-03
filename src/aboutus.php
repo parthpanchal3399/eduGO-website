15db0d126497f75e9c3c7cb6a5220208e5e7cdf8
Parth Panchal <51913848+HtraP1001@users.noreply.github.com>
2019-11-03 22:14:16 +0530

    about us responsive done

--- src/aboutus.php
+++ src/aboutus.php
@@ -32,14 +32,14 @@
         </div>
     
         <div style="padding-top: 50px; padding-bottom: 20px;" class="row">
-            <div class="col-6">
-                <div class="card center" style="width: 70%;">
+            <div class="col-lg-6 col-md-12 about-us-div">
+                <div class="card center about-us-card">
                     <div class="about-us">
                         <img  class="card-img-top" src="images/Parth.jpg" alt="Card image cap">  
                     </div>
                     <div class="card-body">
-                        <h2 class="card-title center">Parth Panchal</h2>
-                        <h6 class="card-subtitle mb-2 text-muted center">Our aim was to make BEST content available at one place &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h6><br><br>
+                        <h2 class="card-title center" style="text-align: center;">Parth Panchal</h2>
+                        <h6 class="card-subtitle mb-2 text-muted center" style="text-align: center;">Our aim was to make BEST content available at one place</h6><br>
                         <div class="icons center">
                             <a href="https://www.facebook.com/HtraP1001"><i class="fab fa-facebook-square"></i></a>
                             <a href="https://www.instagram.com/htrap_1001/"><i class="fab fa-instagram"></i></a>
@@ -49,14 +49,14 @@
                 </div>
             </div>
 
-            <div class="col-6">
-                <div class="card center" style="width: 70%;">
+            <div class="col-lg-6 col-md-12 about-us-div">
+                <div class="card center about-us-card">
                     <div class="about-us">
                         <img  class="card-img-top" src="images/Veer.jpg" alt="Card image cap">  
                     </div>
                     <div class="card-body">
-                        <h2 class="card-title center">Veerapathiramoorthy Pillai</h2>
-                        <h6 class="card-subtitle mb-2 text-muted center">We wanted to reduce student's stress of finding the Best tutorials</h6>
+                        <h2 class="card-title center" style="text-align: center;">Veerapathiramoorthy Pillai</h2>
+                        <h6 class="card-subtitle mb-2 text-muted center" style="text-align: center;">We wanted to reduce student's stress of finding the Best tutorials</h6>
                         <div class="icons center">
                             <a href="https://www.facebook.com/veerapathiramoorthy.pillai"><i class="fab fa-facebook-square"></i></a>
                             <a href="https://www.instagram.com/vo_lambe_naam_wala/"><i class="fab fa-instagram"></i></a>
@@ -73,11 +73,11 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-6">
+            <div class="col-lg-6 col-md-12 about-us-map">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.4973007355625!2d-74.00284058506423!3d40.72908164450808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259921fd9ec47%3A0x731096cd16ad4a1e!2s177a%20Bleecker%20St%2C%20New%20York%2C%20NY%2010012%2C%20USA!5e0!3m2!1sen!2sin!4v1571764642783!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
             </div>
 
-            <div class="col-6">
+            <div class="col-lg-6 col-md-12">
                 
                 <div class="address course-about-heading">
                     <p class="hello">Say hello to us at:</p>
--- src/footer.php
+++ src/footer.php
@@ -2,7 +2,7 @@
     <footer id="footer">
         <div class="container-fluid text-md-left" style="padding-top: 30px;">
             <div class="row">
-                <div class="col-10 footer-left-desc" style="padding-left: 40px;">
+                <div class="col-lg-10 col-md-12 footer-left-desc" style="padding-left: 40px;">
                     <h5 class="foorter-heading">eduGO Tech</h5>
                     <p>Engineering Concepts on the GO!!</p>
                     <p>Start Learning from the best!</p>
@@ -10,7 +10,7 @@
 
                 
 
-                <div class="col-2 footer-links">
+                <div class="col-lg-2 col-md-12 footer-links">
                     <ul class="">
                         <li>
                             <a href="index.php">Home</a>
@@ -33,15 +33,7 @@
         </div>
         <!-- Copyright -->
 
-        <!-- UserDefined JavaScript -->
-        <script src="js/main.js"></script>
-        <!-- JavaScript Libraries -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-        </script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous">
-        </script>
-        <script src="js/bootstrap.min.js"></script>
+        
     </footer>
 
 </body>
